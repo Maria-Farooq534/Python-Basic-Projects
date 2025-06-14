9610639668348c94b97dc9d6c7d88b8844774811
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-14 01:19:25 -0700

    Relational  Operators

--- DataTypes.py
+++ DataTypes.py
@@ -1,22 +1,43 @@
 # Data Types
 
-name = "Maria"
-age = 23
-a = None
-new = False
+# name = "Maria"
+# age = 23
+# a = None
+# new = False
 
-print(type(name))
-print(type(age))
-print(type(a))
-print(type(new))
+# print(type(name))
+# print(type(age))
+# print(type(a))
+# print(type(new))
 
 
-b=4
-d=2
-print(b + d)
-print(b - d)
-print(b * d)
-print(b / d)
+#Arithmetic Operators
 
-print("The division operator in python always results in floating value.")
+# b=4
+# d=2
+# print(b + d)
+# print(b - d)
+# print(b * d)
 
+# print(b ** d)
+# print(b / d)
+
+# print("The division operator in python always results in floating value.")
+
+# print(b % d)
+# print("Modulo operator is used to find the remainder")
+# x=17
+# e=3
+# print(x % e)
+
+# Relational Operators
+
+a = 5
+b = 6
+print(a==b)
+print("Relational operators always result sin a boolean value, means output may be True or False.")
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a>b)

e5542fa9d0eb97a36897b5a2b510dc26bef8261d
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-14 00:43:17 -0700

    Calculating Sum

--- DataTypes.py
+++ DataTypes.py
@@ -3,8 +3,15 @@
 name = "Maria"
 age = 23
 a = None
+new = False
 
 print(type(name))
 print(type(age))
 print(type(a))
+print(type(new))
 
+
+b=4
+d=90
+sum = b+d
+print("The sum of numbers is: ", sum)

173c9f31d3a95ed7ce83a376096dbee0ef3abf6b
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-15 06:36:42 -0700

    input return string value

--- inputFromUser.py
+++ inputFromUser.py
@@ -1,4 +1,9 @@
 #User Input
 
-name = input("Enter your name: ")
-print(f"Welcome {name}")
+# name = input("Enter your name: ")
+# print(f"Welcome {name}")
+
+print("User input always return string data type even if the value is float or integer!")
+
+price = input("Enter price: ")
+print(type(price) , price)

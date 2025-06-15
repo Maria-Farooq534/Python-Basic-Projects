52d719fb5695ea9e977e470934056324a1460a7e
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-15 07:05:16 -0700

    Program to sum two numbers of user input

---
+++ miniProjects.py
@@ -0,0 +1,14 @@
+#Write a program to input 2 numbers and print their sum
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+sum = num1 + num2
+print("The sum is: " , sum)
+
+
+# #Write a program to input side of a square and prints its area.
+
+# side_of_square = int(input("Enter length of side: "))
+
+# area = side_of_square * side_of_square
+# print("The area of square is: " , area)

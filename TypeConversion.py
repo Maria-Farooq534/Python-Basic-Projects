3f2f8343281a8228a08cfc510d74acd73ab9b3f6
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-15 06:22:10 -0700

    Explicit Type Conversion

--- TypeConversion.py
+++ TypeConversion.py
@@ -1,18 +1,18 @@
 #Type Conversion
 
-#Type Conversion 1, implicit conversion. Python interpreter done automatically
-a = 2
-b = 2.5
-sum = a + b
-print(sum)
-
-
-# # Explicit Conversion. manually converting one data type into another.
+# #Type Conversion 1, implicit conversion. Python interpreter done automatically
 # a = 2
-# b = int(2.5)
-# print(type(b))
+# b = 2.5
 # sum = a + b
 # print(sum)
+
+
+# Explicit Conversion. manually converting one data type into another.
+a = 2
+b = int(2.5)
+print(type(b))
+sum = a + b
+print(sum)
 
 
 # #Adding string into float/int

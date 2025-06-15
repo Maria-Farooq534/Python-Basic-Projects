1978b32fcae26b93d2ed2e68bac97139d46ab4f8
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-15 06:41:04 -0700

    input return string value

--- inputFromUser.py
+++ inputFromUser.py
@@ -1,9 +1,20 @@
-#User Input
+##User Input
 
 # name = input("Enter your name: ")
 # print(f"Welcome {name}")
 
-print("User input always return string data type even if the value is float or integer!")
 
-price = input("Enter price: ")
-print(type(price) , price)
+
+## print("User input always return string data type even if the value is float or integer!")
+# price = input("Enter price: ")
+# print(type(price) , price)
+
+
+#Multiple inputs with different data types , but results in string
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+marks = input("Enter marks: ")
+print(type(name), name)
+print(type(age) , age)
+print(type(marks) , marks)
+

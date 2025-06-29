518910e971c9cba5c6027caab2f6cbaefdfbd9f5
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-28 19:38:56 -0700

    Conditional statements

---
+++ ConditionalStatements.py
@@ -0,0 +1,23 @@
+# light = "orange"
+
+# if light == "red":
+#     print("stop")
+# elif light == "green":
+#     print("go")
+# elif light == "yellow":
+#     print("wait")
+# else:
+#     print("There's no light")
+
+# num = 10
+# if num >= 3:
+#     print("Number is greater than 3")
+# elif num >= 4:
+#     print("Number is greater than 4") #In this case, elif will not execute even if it is true
+
+
+num = 10
+if num >= 3:
+    print("Number is greater than 3")
+if num >= 4:
+    print("Number is greater than 4") #If condition will executes even if the first one is true

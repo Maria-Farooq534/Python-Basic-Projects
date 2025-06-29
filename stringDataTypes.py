49d15a3dca9e0889cba57ebebb89aed286eeea5d
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-28 18:06:20 -0700

    Slicing in string

--- stringDataTypes.py
+++ stringDataTypes.py
@@ -16,3 +16,17 @@ My name is Maria \nI am 23 years old
 """
 
 print(c)
+
+c = """
+My name is Maria \tI am 23 years old
+"""
+
+print(c)
+
+
+#Slicing
+
+years = print(age[5:13])
+years = print(age[10: len(age)])
+#Negative Slicing
+year = print(age[-9:-6])

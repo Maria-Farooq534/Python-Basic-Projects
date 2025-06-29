278ef5497ccb9ebfecd52fb1f1f484be2c592686
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-28 19:08:59 -0700

    String related Questions

--- miniProjects.py
+++ miniProjects.py
@@ -20,14 +20,29 @@
 
 
 # #WAP to input 2 floating point numbers and print their average 
+
 # num1 = float(input("Enter num 1: "))
 # num2 = float(input("Enter Number 2: "))
 # average = (num1 + num2)  / 2
 # print(average)
 
-#WAP to input 2 integer numbers a and b. and
-#Print True if a is greater than or equal to b. otherwise print False.
 
-num1 = int(input("num 1: "))
-num2 = int(input("num 2: "))
-print(num1 >= num2)
+# #WAP to input 2 integer numbers a and b. and
+# #Print True if a is greater than or equal to b. otherwise print False.
+
+# num1 = int(input("num 1: "))
+# num2 = int(input("num 2: "))
+# print(num1 >= num2)
+
+
+# #WAP to input user's first name and print its length
+
+# first_name = input("Enter your first name: ")
+# print("Length of your name is:" , len(first_name))
+
+
+#WAP to find the occurance of '$' in  a string
+
+earning = "I have earned $ 150$"
+print(f"The occurance of '$' is: {earning.count("$")}") #we have to find no of occurances, so use count method
+

4be50974177d043c3910dc4ce497e2a14524d5b5
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-06-28 18:56:21 -0700

    string Functions

--- stringDataTypes.py
+++ stringDataTypes.py
@@ -1,32 +1,57 @@
-name = "My name is Maria"
-age = "I am 23 years old"
-#Concatenation
-Maria = name + age
-print(Maria)
+# name = "My name is Maria"
+# age = "I am 23 years old"
+# #Concatenation
+# Maria = name + age
+# print(Maria)
 
-a = "a"
-b = "b"
+# a = "a"
+# b = "b"
 
-c = "a" + " " + "b" # we can print space as well
-print(c)
+# c = "a" + " " + "b" # we can print space as well
+# print(c)
 
-#Escape Sequence Character
-c = """
-My name is Maria \nI am 23 years old
-"""
+# #Escape Sequence Character
+# c = """
+# My name is Maria \nI am 23 years old
+# """
 
-print(c)
+# print(c)
 
-c = """
-My name is Maria \tI am 23 years old
-"""
+# c = """
+# My name is Maria \tI am 23 years old
+# """
 
-print(c)
+# print(c)
 
 
-#Slicing
+# #Slicing
 
-years = print(age[5:13])
-years = print(age[10: len(age)])
-#Negative Slicing
-year = print(age[-9:-6])
+# years = print(age[5:13])
+# years = print(age[10: len(age)])
+# #Negative Slicing
+# year = print(age[-9:-6])
+
+# #String Functions
+
+# python = "i am learning python"
+
+# python1 = print(python.endswith("on")) #True
+# python2 = print(python.capitalize()) #This will make a new string and capitalize it
+
+# python4 = "python learning"
+
+# python4 = python4.capitalize() # Will captilize the string
+# print(python4)
+
+# #find
+# study = "I am studying python"
+# print(study.find("am")) #will return index number 2
+
+# #Count
+
+# count_a = "This function will count the number of occurances of substring"
+# print(count_a.count("s")) #4
+
+#Replace
+replace_function = "This wil replace with a new substring"
+print(replace_function.replace("wil" , "will"))

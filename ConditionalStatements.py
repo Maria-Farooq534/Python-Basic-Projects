96d66100332ad336992e5b7aad7ce0891209a2f7
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-07-03 10:28:42 -0700

    practice work

--- ConditionalStatements.py
+++ ConditionalStatements.py
@@ -16,8 +16,19 @@
 #     print("Number is greater than 4") #In this case, elif will not execute even if it is true
 
 
-num = 10
-if num >= 3:
-    print("Number is greater than 3")
-if num >= 4:
-    print("Number is greater than 4") #If condition will executes even if the first one is true
+# num = 10
+# if num >= 3:
+#     print("Number is greater than 3")
+# if num >= 4:
+#     print("Number is greater than 4") #If condition will executes even if the first one is true
+
+
+#Nesting
+model_accuracy = 97
+if (model_accuracy >= 90):
+    if (model_accuracy >= 96):
+        print("Model is overfitted")
+    else:
+        print("Model is trained well")
+else:
+    print("Model is underfitted")
